2192cc7bf4626eb64d4903998d56580070b59126
1012598167 <chennuo909@163.com>
2023-01-11 08:57:52 +0000

    new test3

--- run_transcoder_debug.sh
+++ run_transcoder_debug.sh
@@ -3,7 +3,7 @@ WORKDIR="/wjn/projects/pe_code/TransCoder"
 #HUGGINGFACE_LOCALS="/data/huggingface_models/"
 HUGGINGFACE_LOCALS="/wjn/pre-trained-lm/"
 #ORIGIN_MODEL_DIR="/data/huggingface_models/"
-ORIGIN_MODEL_DIR="/wjn/projects/code/CodePrompt/save_models"
+ORIGIN_MODEL_DIR="/wjn/pre-trained-lm/codet5-base/"
 export PYTHONPATH=$WORKDIR
 
 MODEL_NAME=${1}
